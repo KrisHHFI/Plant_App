593264365f2f4b581480f562ac4a1b9fc24ee17f
KrisHHFI <kristopherpepperuk@gmail.com>
2025-02-04 00:22:00 +0200

    Three main pages setup, with working nav bar. Runs on Android and IOS.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View } from 'react-native';
 import NavBar from './src/components/NavBar';
 import styles from './src/styling/Styles';
+import ListPage from './src/pages/ListPage';
+import ProfilePage from './src/pages/ProfilePage';
+import SettingsPage from './src/pages/SettingsPage';
 
 function App(): React.JSX.Element {
+  const [currentPage, setCurrentPage] = useState('List');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'List':
+        return <ListPage />;
+      case 'Profile':
+        return <ProfilePage />;
+      case 'Settings':
+        return <SettingsPage />;
+      default:
+        return <ListPage />;
+    }
+  };
+
   return (
     <SafeAreaView style={styles.appSafeAreaView}>
       <View style={styles.appContentView}>
-        <Text>Home page</Text>
+        {renderPage()}
       </View>
-      <NavBar />
+      <NavBar setCurrentPage={setCurrentPage} />
     </SafeAreaView>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from '../styling/Styles';
 
-const NavBar = () => {
+const NavBar = ({ setCurrentPage }: { setCurrentPage: React.Dispatch<React.SetStateAction<string>> }) => {
   return (
     <View style={styles.navBarContainer}>
-      <TouchableOpacity style={styles.navBarButton} onPress={() => console.log('List pressed')}>
+      <TouchableOpacity
+        style={styles.navBarButton}
+        onPress={() => setCurrentPage('List')}>
         <Text>List</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.navBarButton} onPress={() => console.log('Profile pressed')}>
+      <TouchableOpacity
+        style={styles.navBarButton}
+        onPress={() => setCurrentPage('Profile')}>
         <Text>Profile</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.navBarButton} onPress={() => console.log('Settings pressed')}>
+      <TouchableOpacity
+        style={styles.navBarButton}
+        onPress={() => setCurrentPage('Settings')}>
         <Text>Settings</Text>
       </TouchableOpacity>
     </View>

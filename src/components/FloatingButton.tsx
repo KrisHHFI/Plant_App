ab7e40e303b22be75d08315de128f7a3e6b92716
KrisHHFI <kristopherpepperuk@gmail.com>
2025-02-07 15:11:20 +0200

    Floating button, additional styling

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -9,17 +9,19 @@ interface FloatingButtonProps {
 }
 
 const FloatingButton: React.FC<FloatingButtonProps> = ({ text = '', onPress }) => {
+    if (text === '+' || text === '✔' || text === 'x') {
+        return (
+            <TouchableOpacity style={styles.floatingButton} onPress={onPress}>
+                {text === '+' && <Icon name="plus" size={30} color="white" />}
+                {text === '✔' && <Icon name="check" size={30} color="white" />}
+                {text === 'x' && <Icon name="times" size={30} color="white" />}
+            </TouchableOpacity>
+        );
+    }
+
     return (
-        <TouchableOpacity style={styles.floatingButton} onPress={onPress}>
-            {text === '+' ? (
-                <Icon name="plus" size={30} color="white" />
-            ) : text === '✔' ? (
-                <Icon name="check" size={30} color="white" />
-            ) : text === 'x' ? (
-                <Icon name="times" size={30} color="white" />
-            ) : (
-                <Text style={styles.floatingButtonText}>{text}</Text>
-            )}
+        <TouchableOpacity style={[styles.floatingButton, styles.floatingButtonTextContainer]} onPress={onPress}>
+            <Text style={styles.floatingButtonText}>{text}</Text>
         </TouchableOpacity>
     );
 };
